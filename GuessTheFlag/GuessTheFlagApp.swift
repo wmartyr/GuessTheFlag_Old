979135f2abe4316e38697c0c481bf29d0ffe9979
Woodrow Martyr <woodrow.martyr@gmail.com>
2023-09-04 20:03:40 +0930

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Woodrow Martyr on 4/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
